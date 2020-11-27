3796abcbe51b293c8e9ee9f54ca584ff6fbb79f0
mejriwajdi <mejri4wajdi@gmail.com>
2020-11-27 15:32:39 +0100

    Update models.py

--- custom_reg_form/models.py
+++ custom_reg_form/models.py
@@ -11,20 +11,22 @@ class ExtraInfo(models.Model):
     The form that wraps this model is in the forms.py file.
     """
     user = models.OneToOneField(USER_MODEL, null=True, on_delete=models.PROTECT)
-    FAVORITE_EDITOR = (
-        ('vim', 'Vim'),
-        ('emacs', 'Emacs'),
-        ('np', 'Notepad'),
-        ('cat', 'cat > filename'),
+    MEMBER_ORGANIZATION_INSTITUTION = (
+        ('0', 'No'),
+        ('1', 'Yes'),
     )
 
-    favorite_movie = models.CharField(
-        verbose_name="Fav Flick",
+    member_org_inst = models.CharField(
+        verbose_name="Are you a member of an organization/institution",
+        choices=MEMBER_ORGANIZATION_INSTITUTION,
+        blank=False, 
+        max_length=5,
+    )
+    org_inst = models.CharField(
+        verbose_name="Organization/Institution name",
         max_length=100,
     )
-    favorite_editor = models.CharField(
-        verbose_name="Favorite Editor",
-        choices=FAVORITE_EDITOR,
-        blank=True, 
-        max_length=5,
+    tel = models.CharField(
+        verbose_name="Tel",
+        max_length=100,
     )
